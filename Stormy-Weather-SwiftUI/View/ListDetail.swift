3697864272734ad90652a6d42eb60ef453369d9f
Jatin Foujdar <jatin321001@gmail.com>
2024-10-28 20:14:35 +0530

    favorite button on each city commit

--- Stormy-Weather-SwiftUI/View/ListDetail.swift
+++ Stormy-Weather-SwiftUI/View/ListDetail.swift
@@ -1,19 +1,22 @@
 import SwiftUI
 
 struct ListDetail: View {
+    @Environment(ModelData.self) var modelData
     var city: CityWeather
 
+    var cityIndex : Int {
+        modelData.cityWeathers.firstIndex(where: { $0.id == city.id })!
+    }
+    
     var body: some View {
         VStack {
             Text(city.name)
                 .font(.largeTitle)
                 .padding()
-
-            city.image
-                .resizable()
-                .scaledToFit()
-                .frame(height: 200)
-                .padding()
+            
+            CircleImage(image: city.image)
+            .offset(y: 50)
+            .padding(.bottom, 50)
 
             Text("Temperature: \(city.temp)°C")
                 .font(.title2)
@@ -36,5 +39,7 @@ struct ListDetail: View {
 }
 
 #Preview {
-    ListDetail(city: ModelData().cityWeathers[0])
+    let modelData = ModelData()
+    return ListDetail(city: ModelData().cityWeathers[0])
+        .environment(modelData)
 }
